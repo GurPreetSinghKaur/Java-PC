0f43e4273c18f4f5a54e84e2ef3af50691d5486a
GurPreet SinghKaur <gurpreet122p@hotmail.es>
2024-06-17 09:16:13 +0100

    Updated IsValidMobileNumber

--- 1_exercises/IsValidMobileNumber.java
+++ 1_exercises/IsValidMobileNumber.java
@@ -28,6 +28,16 @@ Examples:
 	static boolean solve(String input){
 		
 		// Insert your code here
+
+		//Check if the input has valid minimum length and if it matches the allowed characters
+		if (input.length() < 10 | ! input.matches("//Q+.?//E[0-9]"))  return false;
+
+		String[] temp = input.split("7");
+
+		if (temp[0].contains("0") | temp[0].contains("+44") | temp[0].contains("0044")) return true;
+
+		return false;
+		
 						
 	}
 	
