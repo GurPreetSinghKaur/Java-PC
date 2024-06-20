0a1a7c9ca952b3a0b65e665e32051556e8a5f67d
GurPreet SinghKaur <gurpreet122p@hotmail.es>
2024-06-20 11:44:11 +0100

    added comments

--- junit.testing/src/main/java/gurpreet/junit/App.java
+++ junit.testing/src/main/java/gurpreet/junit/App.java
@@ -10,4 +10,7 @@ public class App
     {
         System.out.println( "Hello World!" );
     }
+    //Need to add all files to main.java.gurpreet.junit package and then remove its main methods
+    //then need to add testing in test.java.gurpreet.junit
+    //add dependencies to run JUNIT
 }
