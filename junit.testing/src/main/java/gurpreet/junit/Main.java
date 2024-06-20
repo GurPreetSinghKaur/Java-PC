eae966de2ca4ba670fd3d9ddc6ec2b60fd71de4a
GurPreet SinghKaur <gurpreet122p@hotmail.es>
2024-06-21 00:09:45 +0100

    set up plugins and folders

--- junit.testing/src/main/java/gurpreet/junit/Main.java
+++ junit.testing/src/main/java/gurpreet/junit/Main.java
@@ -4,7 +4,7 @@ package gurpreet.junit;
  * Hello world!
  *
  */
-public class App 
+public class Main
 {
     public static void main( String[] args )
     {
@@ -13,5 +13,4 @@ public class App
     //Need to add all files to main.java.gurpreet.junit package and then remove its main methods
     //then need to add testing in test.java.gurpreet.junit
     //add dependencies to run JUNIT
-    //add executable .jar dependencies and configurations in pom.xml
-}
+ }
