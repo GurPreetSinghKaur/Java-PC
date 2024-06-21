789aa54cbdd14aa11a9b45d46fdb867a4da7e0d6
GurPreet SinghKaur <gurpreet122p@hotmail.es>
2024-06-21 13:48:57 +0100

    Fixed POM file dependencies related to JUnit

--- junit.testing/src/test/java/gurpreet/junit/TestExercises.java
+++ junit.testing/src/test/java/gurpreet/junit/TestExercises.java
@@ -13,21 +13,11 @@ import org.junit.jupiter.api.Test;
  */
 public class TestExercises
 {
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-       // assertTrue( true );
-       // Assertions.assertThat(true).isTrue();
-    }
     @Test
     public void FlipBooleans(){
-     //Assertions.assertThat(  FlipBooleans.tryInput(new boolean[]{ true, false, false, true}, new boolean[]{ false, true, true, false})).isTrue();
+    Assertions.assertThat(  FlipBooleans.solve(new boolean[]{ true, false, false, true})).isEqualTo( new boolean[]{ false, true, true, false});
    // Assertions.assertThat(FlipBooleans.tryInput(new boolean[]{ false, false, false, false, false}, new boolean[]{ true, true, true, true, true})).isTrue();
 
     }
-
 
 }
--- junit.testing/src/test/java/gurpreet/junit/exercises/FlipBooleansTest.java
+++ junit.testing/src/test/java/gurpreet/junit/exercises/FlipBooleansTest.java
@@ -7,7 +7,6 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class FlipBooleansTest {
     private static Stream<Arguments> inputsAndResults() {
-
         return Stream.of(
                 Arguments.of(new boolean[]{ true, false, false, true}, new boolean[]{ false, true, true, false}),
                 Arguments.of(new boolean[]{ false, false, false, false, false}, new boolean[]{ true, true, true, true, true}),
