21c74584b443f00008b53cf0973d1f100710bd78
GurPreet SinghKaur <gurpreet122p@hotmail.es>
2024-06-23 21:49:57 +0100

    Created a test

--- junit.testing/src/test/java/gurpreet/junit/exercises/FindFirstDentistTest.java
+++ junit.testing/src/test/java/gurpreet/junit/exercises/FindFirstDentistTest.java
@@ -1,9 +1,13 @@
 package gurpreet.junit.exercises;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import java.util.stream.Stream;
+import java.util.stream.*;
+import java.util.*;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class FindFirstDentistTest {
@@ -11,20 +15,22 @@ public class FindFirstDentistTest {
 
  private static Stream<Arguments> listOfPerson() {
         return Stream.of(
+
 		 Arguments.of(new ArrayList<>(), null),
 		 Arguments.of(new ArrayList<Person>(Arrays.asList(new Person("Callum", false))), null),
 		 Arguments.of(new ArrayList<Person>(Arrays.asList(new Person("Callum", false), new Person("Diane", false))), null),
 		 Arguments.of(new ArrayList<Person>(Arrays.asList(new Person("Callum", false), new Person("Diane", false), new Person("Egbert", true))), "Egbert"),
 		 Arguments.of(new ArrayList<Person>(Arrays.asList(new Person("Callum", false), new Person("Diane", false), new Person("Egbert", true), new Person("Francesco", true))), "Egbert")
-	}
+
 
 
         );
     }
 @ParameterizedTest
 @MethodSource("listOfPerson")
-public void findFirstDentistTest(ArrayList<Person> a, ArrayList<Person> b){
-	  assertEquals(FindFirstDentist.solve(a), b);
+@DisplayName("Find the first dentist from the String ArrayList, else returns null")
+public void findFirstDentistTest(ArrayList<Person> a, String b){
+	Assertions.assertEquals(FindFirstDentist.solve(a), b);
 }
         
 }
