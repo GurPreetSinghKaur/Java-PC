b340dd5fc9172cfaf42cf76c0e6d16fd37cb2e56
GurPreet SinghKaur <gurpreet122p@hotmail.es>
2024-06-23 22:12:50 +0100

    Removed unnecessary code

--- junit.testing/src/main/java/gurpreet/junit/exercises/GetPalindromes.java
+++ junit.testing/src/main/java/gurpreet/junit/exercises/GetPalindromes.java
@@ -7,7 +7,6 @@ Write a function that takes a list of words and returns a list containing
 all of the words that are palindromes (the same word forwards and backwards, e.g. 'pip').
 
  */
-
 public class GetPalindromes {
 	
 	static ArrayList<String> solve(ArrayList<String> input){
@@ -21,35 +20,8 @@ public class GetPalindromes {
 			if (sb.reverse().toString().equals(input.get(i))) result.add(input.get(i)); 
 			
 		}
-
-
-
-
 		return result;
 						
-	}
-	
-	public static void main(String[] args) {
-
-		tryInput(new ArrayList<>(), new ArrayList<>());
-
-		tryInput(new ArrayList<>(List.of("racecar")), new ArrayList<>(List.of("racecar")));
-
-		tryInput(new ArrayList<>(List.of("zracecar")), new ArrayList<>());
-
-		tryInput(new ArrayList<>(List.of("dog", "dud", "car", "mum")), new ArrayList<>(List.of("dud", "mum")));
-
-		tryInput(new ArrayList<>(List.of("apple", "orange", "banana", "mum")), new ArrayList<>(List.of("mum")));
-				
-	}
-
-	static void tryInput(ArrayList<String> input, ArrayList<String> expectedOutput){
-		ArrayList<String> result = GetPalindromes.solve(input);
-		if(result.equals(expectedOutput)){
-			System.out.println("✅ SUCCESS");
-		}else{
-			System.out.println("❌ FAILED");
-		}
 	}
 
 }
